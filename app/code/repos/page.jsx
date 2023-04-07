08826127c5eea4f03f843097683001a82d8ff156
Allan Kiche <tle2819@students.mu.ac.ke>
2023-04-07 23:57:19 +0300

    page3

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import {FaStar, FaCodeBranch, FaEye} from 'react-icons/fa';
+import Link from 'next/link';
+
+
 async function fetchRepos() {
       const res = await fetch('https://api.github.com/users/kiche-allan/repos');
       const repos = await res.json();
@@ -8,9 +12,17 @@ async function fetchRepos() {
 const ReposPage = async() => {
 
     const repos = await fetchRepos();
-    console.log(repos);
+    
   return (
-    <div>
+    <div className = "repos-container">
+        <h2>Repositories</h2>
+        <ul className="repo-list">
+            {repos.map((repo) => (
+                <li href = {`/code/repos/${repo.name}`}>
+                    <h3>{repo.name}</h3>
+                </li>
+            ))}
+        </ul>
         {repos[0].name}
     </div>
   )
