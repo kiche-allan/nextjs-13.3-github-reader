238c95f6515f2353eb3557b7b2e09e966551a469
Allan Kiche <tle2819@students.mu.ac.ke>
2023-04-11 18:22:24 +0300

    page.jsx

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -6,19 +6,24 @@ import Link from 'next/link';
 
 async function fetchRepos() {
       const res = await fetch('https://api.github.com/users/kiche-allan/repos');
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
       const repos = await res.json();
       return repos;
 }
 const ReposPage = async() => {
 
     const repos = await fetchRepos();
+    return repos;
     
   return (
     <div className = "repos-container">
         <h2>Repositories</h2>
+        
         <ul className="repo-list">
             {repos.map((repo) => (
-                <li href = {`/code/repos/${repo.name}`}>
+                <li key = {repo.id}>
+                <Link href = {`/code/repos/${repo.name}`}>
                     <h3>{repo.name}</h3>
                     <p>{repo.description}</p>
                     <div className="repo-details">
@@ -26,9 +31,11 @@ const ReposPage = async() => {
                         <span><FaCodeBranch /> {repo.forks_count}</span>
                         <span> <FaEye /> {repo.watchers_count}</span>
                     </div>
+                </Link>
                 </li>
             ))}
         </ul>
+       
         {repos[0].name}
     </div>
   )
