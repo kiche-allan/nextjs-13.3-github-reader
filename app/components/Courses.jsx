33156a7ebb6e62a47adae91285a8fcc8c17b18a6
Allan Kiche <tle2819@students.mu.ac.ke>
2023-04-12 13:29:23 +0300

    json3

--- app/components/Courses.jsx
+++ app/components/Courses.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import Link from "next/link"
 
+//this will be a server component so use async
 
+async function fetchCourses() {
+    const response = await fetch("http://localhost:3000/api/courses");
+    const courses = await response.json();
+    return courses;
+}
 const Courses = () => {
   return (
     <div>Courses</div>
